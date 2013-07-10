6da8ca224dc241462baae3f92273520518ddce2b
Frank Hamand <fhamand@geneity.co.uk>
2013-07-10 14:27:25 +0100

    tried multiprocessing

--- shortbread.py
+++ shortbread.py
@@ -18,7 +18,7 @@ def shortbread(short, bread, word_list, letters, depth_first):
 					word_list.remove(new_word)
 					yield new_word, path + [new_word]
 
-	def words_gen(starting_word, word_list, depth_first):
+	def words_gen(starting_word, word_list, depth_first, results_queue):
 		# defensive copy of word list
 		word_list = set(word_list)
 		word_list.remove(starting_word)
@@ -27,7 +27,11 @@ def shortbread(short, bread, word_list, letters, depth_first):
 
 		# The function will return when new_words throws StopIteration
 		while True:
-			next_word, path = new_words.next()
+			try:
+				next_word, path = new_words.next()
+			except StopIteration:
+				results_queue.close()
+				return
 
 			if depth_first:
 				new_words = itertools.chain(
@@ -40,19 +44,34 @@ def shortbread(short, bread, word_list, letters, depth_first):
 					mutate_word(next_word, path, word_list)
 				)
 
-			yield next_word, path
+			results_queue.put((next_word, path), False)
 
-	# Double ended breadth first search.
-	for (lword, lpath), (rword, rpath) in \
-	itertools.izip(words_gen(short, word_list, depth_first), 
-				   words_gen(bread, word_list, depth_first)):
+	from multiprocessing import Process, Queue
+	start_results_queue = Queue()
+	end_results_queue = Queue()
+	start_proc = Process(target=words_gen, args=(short, word_list, depth_first, start_results_queue))
+	end_proc = Process(target=words_gen, args=(bread, word_list, depth_first, end_results_queue))
+
+	start_proc.start()
+	end_proc.start()
+
+	while start_proc.is_alive() and end_proc.is_alive():
+		(lword, lpath), (rword, rpath) = start_results_queue.get(), end_results_queue.get()
+
 		if lword in rpaths:
+			start_proc.terminate()
+			end_proc.terminate()
 			return lpath + rpaths[lword][::-1][1:]
 		if rword in lpaths:
+			start_proc.terminate()
+			end_proc.terminate()
 			return lpaths[rword] + rpath[::-1][1:]
 
 		lpaths[lword] = lpath
 		rpaths[rword] = rpath
+
+	start_proc.terminate()
+	end_proc.terminate()
 
 if __name__ == '__main__':
 	"""
@@ -104,9 +123,9 @@ if __name__ == '__main__':
 		exit(0)
 
 	assert(len(short) == len(bread)), "Both words must be the same length"
+	word_len = len(short)
 
 	words_content = open('/usr/share/dict/british-english').read()
-	word_list = set(word for word in words_content.split() 
-		                          if len(word) == len(bread))
+	word_list = list(w for w in words_content.split() if len(w) == word_len)
 
 	print "path is", shortbread(short, bread, word_list, options.letters, options.depth_first)
